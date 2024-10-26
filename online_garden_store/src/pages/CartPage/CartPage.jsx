c022b3efe63e1ba19456ed4ee05a4c245525ceb9
Getitdown <yekaterina.kiel@gmail.com>
2024-10-26 16:17:26 +0200

    add shopping page

--- online_garden_store/src/pages/CartPage/CartPage.jsx
+++ online_garden_store/src/pages/CartPage/CartPage.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
-import FilterForm from '../../components/FilterForm/FilterForm'
+import CartContainer from '../../components/CartContainer/CartContainer'
+import { useSelector } from 'react-redux'
+import s from './CartPage.module.css'
+import { Link } from 'react-router-dom'
+import Footer from '../../components/Footer/Footer'
 
 export default function CartPage() {
+
+  const cartState = useSelector(store => store.cart)
+
   return (
     <div>
-      CartPage</div>
+        <div className={s.cart_page}>
+        
+          <div>
+            <h2>Shopping cart</h2>
+            <div className={s.separator}></div>
+            <Link to='/categories' className={s.back_to_categories}>Back to the store</Link>
+          </div>
+
+        {
+          cartState.length === 0
+          ? <p>Looks like you have no items in your basket currently.</p>
+          : <div>
+            <CartContainer cartState={cartState}/>
+          </div>
+        }
+        <Link to='/categories'>
+          <button>Continue Shopping</button>
+        </Link>
+        
+        </div>
+        <Footer/>
+      </div>
   )
 }
--- online_garden_store/src/pages/SingleProductPage/SingleProductPage.jsx
+++ online_garden_store/src/pages/SingleProductPage/SingleProductPage.jsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom'
 import { getSingleProduct } from '../../requests/products'
 import s from './SingleProductPage.module.css'
 import { addProductToCartAction } from '../../store/cartReducer'
+import Footer from '../../components/Footer/Footer'
 
 export default function SingleProductPage() {
 
@@ -34,40 +35,43 @@ export default function SingleProductPage() {
 
     const discountPercent = Math.round(((price - discont_price) / price) * 100);
   return (
-    <div className={s.layout_container}>
-    {
-        singleProductState.status === 'loading' 
-        ? 'Product info is loading' 
-        : <div className={s.single_product_card}>
-            
-                <img src={`http://localhost:3333${image}`} alt={title} />
-            
-            <div className={s.product_page}>
-                <div>
-                    <h3>{title}</h3>
-                </div>
-                <div className={s.price_single_product}>
-                    <p>${price}</p>
-                    {discont_price ? <><p>${discont_price}</p> 
-                    <span>{-discountPercent}%</span>
-                    
-                    </> : null}
-                    
-                </div>
-                <div className={s.quantity_wrapper}>
-                    <div onClick={decrCount}>-</div>
-                    <p>{count}</p>
-                    <div onClick={incrCount}>+</div>
-                    <button onClick={() => dispatch(addProductToCartAction)}>Add to cart</button>
-                </div>
-                <div className={s.product_description}>
-                    <p>Desciption</p>
-                    <p>{description}</p>
-                    <p>Read more</p>
+    <div>
+        <div className={s.layout_container}>    
+        {
+            singleProductState.status === 'loading' 
+            ? 'Product info is loading' 
+            : <div className={s.single_product_card}>
+                
+                    <img src={`http://localhost:3333${image}`} alt={title} />
+                
+                <div className={s.product_page}>
+                    <div>
+                        <h3>{title}</h3>
+                    </div>
+                    <div className={s.price_single_product}>
+                        <p>${price}</p>
+                        {discont_price ? <><p>${discont_price}</p> 
+                        <span>{-discountPercent}%</span>
+                        
+                        </> : null}
+                        
+                    </div>
+                    <div className={s.quantity_wrapper}>
+                        <div onClick={decrCount}>-</div>
+                        <p>{count}</p>
+                        <div onClick={incrCount}>+</div>
+                        <button onClick={() => dispatch(addProductToCartAction)}>Add to cart</button>
+                    </div>
+                    <div className={s.product_description}>
+                        <p>Desciption</p>
+                        <p>{description}</p>
+                        <p>Read more</p>
+                    </div>
                 </div>
             </div>
-        </div>
-    }
+        }
+    </div>
+    <Footer/>
 </div>
   )
 }
