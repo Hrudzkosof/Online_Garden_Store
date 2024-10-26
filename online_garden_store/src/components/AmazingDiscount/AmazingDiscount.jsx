35188a94aa5530ff5d62e77993801dbbc2468bda
Lazizahon <lasisa2003@gmail.com>
2024-10-26 18:34:29 +0200

    amazigDiscounts

---
+++ online_garden_store/src/components/AmazingDiscount/AmazingDiscount.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import DiscountProducts from '../DiscountProducts/DiscountProducts'
+import s from './index.module.css'
+export default function AmazingDiscount() {
+  const [showDiscountProducts, setShowDiscountProducts] = useState(false)
+
+  const handleClick = () => {
+    setShowDiscountProducts(true) 
+  }
+  return (
+    <div>
+      {!showDiscountProducts ? (
+        <div className={s.amazingDiscountImg}>
+          <div className={s.leer}></div>
+          <div className={s.discountText}>
+            <h1 className={s.amazingDiscountH}>Amazing Discounts on Garden Products!</h1>
+            <button className={s.amazingDiscountButton} onClick={handleClick}>Check out</button>
+          </div>
+        </div>
+      ) : (
+        <DiscountProducts /> 
+      )}
+    </div>
+ )    
+}
