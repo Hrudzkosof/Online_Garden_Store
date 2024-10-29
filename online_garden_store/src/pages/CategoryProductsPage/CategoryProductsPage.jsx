cb34249b183373343f595f3ca2e9ed705df33c8a
Viktoriia <viktorii.vasilenko@gmail.com>
2024-10-29 15:55:28 +0200

    chenge

--- online_garden_store/src/pages/CategoryProductsPage/CategoryProductsPage.jsx
+++ online_garden_store/src/pages/CategoryProductsPage/CategoryProductsPage.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import AllProductCard from '../../components/AllProductCard/AllProductCard';
-import { getCategoryProducts} from '../../requests/categoryProducts_req';
+import { useDispatch } from 'react-redux';
+import { addProductToCartAction } from '../../store/cartReducer'; 
+import { getCategoryProducts } from '../../requests/categoryProducts_req';
 import s from './CategoryProductsPage.module.css'; 
 
 export default function CategoryProductsPage() {
-  const { id } = useParams(); 
+  const { id } = useParams();
   const [products, setProducts] = useState([]);
   const [category, setCategory] = useState(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     getCategoryProducts(id).then(data => {
@@ -15,6 +18,10 @@ export default function CategoryProductsPage() {
       setProducts(data.data);
     });
   }, [id]);
+
+  const handleAddToCart = (product) => {
+    dispatch(addProductToCartAction(product));
+  };
 
   return (
     <div>
@@ -25,14 +32,26 @@ export default function CategoryProductsPage() {
       )}
       <div className={s.productsContainer}>
         {products.map((product) => (
-          <AllProductCard 
-            key={product.id} 
-            id={product.id} 
-            image={product.image} 
-            title={product.title} 
-            price={product.price} 
-            discont_price={product.discont_price} 
-          />
+          <div key={product.id} className={s.productWrapper}>
+            <AllProductCard 
+              id={product.id} 
+              image={product.image} 
+              title={product.title} 
+              price={product.price} 
+              discont_price={product.discont_price} 
+            />
+            <button 
+              onClick={() => handleAddToCart({
+                id: product.id,
+                image: product.image,
+                title: product.title,
+                price: product.price,
+              })}
+              className={s.addToCartButton}
+            >
+              Add to cart
+            </button>
+          </div>
         ))}
       </div>
     </div>
